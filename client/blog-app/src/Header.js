41593c4787db6ff5afe07c894c562dbb75990e51
verhaq <haqverdah98@gmail.com>
2024-02-14 21:23:44 -0600

    second commit

---
+++ client/blog-app/src/Header.js
@@ -0,0 +1,12 @@
+import {Link} from "react-router-dom"
+
+export default function Header () {
+    return (    
+    <header>
+        <Link to="/" className="logo">My Blog</Link>
+        <nav>
+          <Link to="/login">Login</Link>
+          <Link to="">Register</Link>
+        </nav>
+      </header>);
+}
---
+++ client/blog-app/src/Post.js
@@ -0,0 +1,15 @@
+export default function Post() {
+    return (    <div className="post">
+    <div className="image">
+      <img src="https://sm.ign.com/ign_za/news/o/one-piece-/one-piece-fans-speculate-over-what-chopper-could-look-like-i_bgxg.jpg"></img>
+    </div>
+    <div className="texts">
+      <h2>blahblahblah</h2>
+      <p className='info'>
+        <a className='author'>ya mama</a>
+        <time>2024-02-12 15:35</time>
+      </p>
+      <p className='summary'>Today yapayapyapyap</p>
+    </div>
+  </div>);
+}
